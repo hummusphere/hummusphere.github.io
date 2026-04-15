6b8090bb8748dad3014b6df51468dc95d793f9ac
hummus <humdanq7270@yahoo.com>
2026-04-15 23:14:40 +0000

    Add files via upload

--- scripts/blog.js
+++ scripts/blog.js
@@ -1,5 +1,9 @@
 function back_to_blog_list(blog, blog_list, title, content, back) {
-  location.hash = blog.id;
+  history.pushState(
+    "",
+    document.title,
+    window.location.pathname + window.location.search,
+  );
   title.innerHTML = "Blog";
   content.innerHTML = "";
   blog_list.style.display = "block";
@@ -8,7 +12,7 @@ function back_to_blog_list(blog, blog_list, title, content, back) {
 
 function update_blog_content(id, blog, blog_list) {
   const title = document.getElementById("blog-header");
-  const content = document.getElementById("blog-content").firstChild;
+  const content = document.getElementById("blog-content");
   const back = document.getElementsByClassName("back-to-blogs")[0];
 
   location.hash = blog.id;
@@ -21,21 +25,31 @@ function update_blog_content(id, blog, blog_list) {
 
 async function fetch_blog_content() {
   const blog_list = document.getElementById("blog-list");
-
   const data = await fetch("blog.json");
   const blogs = await data.json();
+
+  var check_hash = false;
+  blog_list.style.display = "none";
 
   blogs.forEach((blog) => {
     const blog_item = document.createElement("li");
 
     blog_item.className = "blog-item";
     blog_item.innerHTML = blog.date + " - " + "<a>" + blog.title + "</a>";
-
     blog_item.onclick = () => update_blog_content(blog.id, blog, blog_list);
-
     blog_list.appendChild(blog_item);
   });
-  return b;
+
+  blogs.forEach((blog) => {
+    if (window.location.hash && window.location.hash.slice(1) == blog.id) {
+      check_hash = true;
+      update_blog_content(blog.id, blog, blog_list);
+    }
+  });
+
+  if (check_hash == false) {
+    blog_list.style.display = "block";
+  }
 }
 
 window.onload = function () {

2cdda470da75ae2ed9ed7ddb738080fc7a3c5bf9
hummusphere <hummusphere@users.noreply.github.com>
2026-07-27 16:07:16 -0600

    added copy url button to blog

---
+++ scripts/blog-post.js
@@ -0,0 +1,17 @@
+const clip_svg = "<path d=\"M4.715 6.542 3.343 7.914a3 3 0 1 0 4.243 4.243l1.828-1.829A3 3 0 0 0 8.586 5.5L8 6.086a1 1 0 0 0-.154.199 2 2 0 0 1 .861 3.337L6.88 11.45a2 2 0 1 1-2.83-2.83l.793-.792a4 4 0 0 1-.128-1.287z\"/><path d=\"M6.586 4.672A3 3 0 0 0 7.414 9.5l.775-.776a2 2 0 0 1-.896-3.346L9.12 3.55a2 2 0 1 1 2.83 2.83l-.793.792c.112.42.155.855.128 1.287l1.372-1.372a3 3 0 1 0-4.243-4.243z\"/>"
+const checkmark_svg = "<path d=\"M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0m-3.97-3.03a.75.75 0 0 0-1.08.022L7.477 9.417 5.384 7.323a.75.75 0 0 0-1.06 1.06L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0 0-.01-1.05z\"/>"
+
+function main() {
+    const share_button = document.getElementById("blog-share-link");
+    share_button.addEventListener("click", function(){
+        navigator.clipboard.writeText(document.URL.split("#")[0]);
+        share_button.innerHTML = checkmark_svg;
+        setTimeout(() => {
+            share_button.innerHTML = clip_svg;
+        }, 500);
+    })
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+    main();
+});
